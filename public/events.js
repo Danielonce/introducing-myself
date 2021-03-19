44fda8b088eb40c79c58f79f859e1f6c1c46e41c
Daniel <montalvadaniel@outlook.com>
2021-03-18 22:17:01 -0300

    social

--- public/events.js
+++ public/events.js
@@ -1,10 +1,17 @@
 const videoBackground = document.querySelector('.videoBackground');
+const contact = document.querySelector('.contact');
+const btn = document.querySelector('.btn');
+
 
 const screenSizeEval = () => {
   if (window.innerWidth > 480) {
     videoBackground.setAttribute("src", "obeleye.mp4");
-  } else videoBackground.setAttribute("src", "obelisco.mp4");
+  } else videoBackground.setAttribute("src", "obeleye-mobile.mp4");
 }
+
+btn.addEventListener('click', () => {
+  contact.classList.toggle('social')
+})
 
 window.addEventListener('resize', () => screenSizeEval())
 window.addEventListener('DOMContentLoaded', () => screenSizeEval())
